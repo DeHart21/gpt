92adcf5d81b153cb7d0b02ff889d6f7591ff416b
DeHart21 <dennis.hart.2021@gmail.com>
2023-03-29 15:53:16 -0500

    Feat: Add retain histroy to gpt and gpt4

--- BashScripts/gpt_user_input.sh
+++ BashScripts/gpt_user_input.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-echo "Enter your message:"
-read user_input
+# Initialize an empty array to store conversation history
+conversation_history=()
 
-response=$(curl -s https://api.openai.com/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-     "model": "gpt-3.5-turbo",
-     "messages": [{"role": "user", "content": "'"${user_input}"'"}],
-     "temperature": 0.7
-   }')
+while true; do
+  echo "Enter your message:"
+  read user_input
 
-echo $response | jq '.choices[0].message.content'
+  # Add the user's message to the conversation history
+  conversation_history+=('{"role": "user", "content": "'"${user_input}"'"}')
+
+  # Prepare the conversation history string for the API call
+  messages=$(IFS=,; echo "[${conversation_history[*]}]")
+
+  response=$(curl -s https://api.openai.com/v1/chat/completions \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $OPENAI_API_KEY" \
+    -d '{
+       "model": "gpt-3.5-turbo",
+       "messages": '"${messages}"',
+       "temperature": 0.7
+     }')
+
+  ai_response=$(echo $response | jq '.choices[0].message.content')
+
+  # Add the AI's response to the conversation history
+  conversation_history+=('{"role": "assistant", "content": "'"${ai_response//\"}"'"}')
+
+  echo "AI Response: $ai_response"
+done
 
